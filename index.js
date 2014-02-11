8a5af930fe74389ec7de6dfe60eaed0d0ea2c1aa
Ben Xu <bjfuxuben@gmail.com>
2014-02-11 14:15:49 +0000

    新建，导入

---
+++ index.js
@@ -0,0 +1,179 @@
+﻿updateTime();
+addScrollListener(window, wheelHandle);
+document.onkeydown = keyDownHandle;
+window.onload = onReload;
+window.onresize = onReload;
+
+if(isMobileDevice()) {//只针对移动设备做的事情
+	//兼容iPhone和android的屏幕旋转事件，function() { alert(window.orientation);}
+	var supportsOrientationChange = "onorientationchange" in window,
+	orientationEvent = supportsOrientationChange ? "orientationchange" : "resize";
+	window.addEventListener(orientationEvent, onReload, false);
+	
+	window.addEventListener('touchstart', onTouchStart, false);
+	window.addEventListener('touchmove', onTouchMove, false);
+	window.addEventListener('touchend', onTouchEnd, false);
+	//window.addEventListener('touchcancel', onTouchCancel, false);	
+}
+
+
+
+/**
+ * 每个页面的宽度和高度
+ */
+var pagewidth, pageheight;
+getHeightAndWidth();
+
+/**
+ * 页面数
+ */
+var pagenum = lists.length;
+
+/**
+ * 默认页面的页面号
+ */
+var curpagenum = 4;
+
+function getHeightAndWidth() {
+	pagewidth = window.innerWidth - 20;
+	pageheight = window.innerHeight - 50;
+	if(!isMobileDevice()) {
+		//移动设备一般都是全屏，所以在宽度上不用减，非移动设备多留点边以便更加美观
+		pagewidth -= 30;
+	}
+}
+
+/**
+ * 填写生成一个searchtable的html内容
+ * @param se1
+ * @param se2
+ * @returns {String}
+ */
+function getSearchtable(se1, se2) {
+	var html = '<table class = "searchtable"><tr>';
+	
+	html += '<td align="center"><form action="';
+	html += se1.url + '" method="get"><input type="text" name="' + se1.name + '"></input><button>' + se1.title + '</button>';
+	html += '</form></td>';
+	
+	if(pagewidth < 800) {
+		html += '</tr><tr>';
+	}
+	
+	html += '<td align="center"><form action="';
+	html += se2.url + '" method="get"><input type="text" name="' + se2.name + '"></input><button>' + se2.title + '</button>';
+	html += '</form></td>';
+	
+	html += '</tr></table>';
+	return html;
+}
+
+/**
+ * 填写生成一个page div的html内容
+ * @param title 页面的标题
+ * @param searchtable 搜索引擎table的html
+ * @param list 网址列表
+ * @returns {String}
+ */
+function renderpage(title, searchtable, list) {
+	var html = '<div class = "page">';
+	html += '<h1 align = "center">' + title + '</h1>';
+	html += searchtable;
+	html += '<table class = "maintable"><tr>';
+	
+	var itemNumInALine;
+	if(pagewidth >= 1280) {
+		itemNumInALine = 5;
+	}else if(pagewidth >= 1000) {
+		itemNumInALine = 4;
+	}else if(pagewidth >= 800){
+		itemNumInALine = 3;
+	}else if(pagewidth >= 600){
+		itemNumInALine = 2;
+	}else {
+		itemNumInALine = 1;
+	}
+	
+	for(var i = 0; i < list.length; i++) {
+		if(i > 0 && i % itemNumInALine == 0) {
+			html += "</tr><tr>";
+		}
+		var item = list[i];
+		html += '<td><a href="';
+		html += item.url + '">' + item.name;
+		html += "</a></td>";
+	}
+	html += "</tr></table>";
+	html += "</div>";
+	return html;
+}
+
+function onReload() {
+	getHeightAndWidth();
+	updatePageCont();
+	updatePageSize();
+	updateCSS();
+//	var mytest = document.getElementById('mytest'); 
+//	mytest.innerHTML = pagewidth + "x" + pageheight;
+}
+
+function updatePageCont() {
+	var html = '';
+	for(var i = 0; i < pagenum; i++) {
+		var item = lists[i];
+		var searchtable = getSearchtable(item.searchengine[0], item.searchengine[1]);
+		html += renderpage(item.title, searchtable, item.list);
+//		render("content" + i, lists[i]);
+	}
+	html += '<div class="clear"></div>';
+	var pagecont = document.getElementById('pagecont'); 
+	pagecont.innerHTML = html;
+}
+
+function updatePageSize() {
+	var wrapper = document.getElementById('wrapper');
+	wrapper.style.width = pagewidth + "px";
+	wrapper.style.height = pageheight + "px";
+
+	var pagewindow = document.getElementById('pagewindow');
+	pagewindow.style.width = pagewidth + "px";
+	
+	var pagecont = document.getElementById('pagecont'); 
+	pagecont.style.width = pagenum * pagewidth + "px";
+	pagecont.style.left = (1 - curpagenum) * pagewidth + "px";
+	
+	var bottom = document.getElementById('bottom');
+	bottom.style.width = pagewidth + "px";
+	
+	updateCSS();
+}
+
+function updateCSS() {
+	var sheet = document.styleSheets[0];
+	var rules = sheet.cssRules || sheet.rules;
+	var tablerule = rules[0];
+	var pagerule = rules[1];
+	tablerule.style.width = pagewidth + "px";
+	pagerule.style.width = pagewidth + "px";
+
+	var wrapperrule = rules[2];
+	var fontsize = computeFontSize();
+	wrapperrule.style.fontSize = fontsize + "px"; 
+}
+
+function computeFontSize() {
+	var refer;
+	if(pagewidth * 0.75 < pageheight) {
+		refer = pagewidth * 0.75;
+	}else {
+		refer = pageheight;
+	}
+	var ret = refer / 25;
+	if(ret < 15) {
+		ret = 15;
+	}
+	if(ret > 30) {
+		ret = 30;
+	}
+	return ret;
+}
---
+++ scroll.js
@@ -0,0 +1,172 @@
+﻿/**
+ * 标记是否正在做切屏动画
+ */
+var animateflag = false;
+
+/**
+ * 将一个"***px"型的字符串转换成数值
+ */
+function getvalue(str) {
+	return parseInt(str.substr(0, str.length - 2));
+}
+
+/**
+ * 判断滚轮事件类型，大于0表示向上滚，小于0表示向下滚
+ */
+function wheeltype(e) {
+    if(e.wheelDelta) { // IE, KHTML, Opera
+        return e.wheelDelta;
+    } else { // Gecko
+        return -e.detail;
+    }				
+}
+
+/**
+ * 切屏的函数
+ * @param flag 指示切屏方向的标记，1表示上一页，-1表示下一页
+ */
+function pagechange(flag) {
+	if(animateflag) {//正在切屏，则忽略本次请求
+		return ;
+	}
+	/*
+	 * 获得一屏的宽度
+	 */
+	var win = document.getElementById("pagewindow");
+	var width = getvalue(win.style.width);
+
+	var cont = document.getElementById("pagecont");
+	var left = getvalue(cont.style.left);
+	var bound = getvalue(cont.style.width) - width;
+	
+	var s = left;
+	var e = left + flag * width;
+	var step = width / 1000;
+	step = step * step * 10 * flag;
+	
+	if(e <= 0 && e >= -bound) {
+		animateflag = true;
+		animate(s, e, step);
+	}
+	
+	if(e > 0) {
+		e = 0;
+	}
+	if(e < -bound) {
+		e = -bound;
+	}
+	cont.style.left = e + "px";
+}
+
+/**
+ * 做一个将cont.style.left从s变至e的动画
+ * @param s
+ * @param e
+ * @param step 每个单位时间移动的步长
+ */
+function animate(s, e, step) {
+	if((step > 0 && s >= e) || (step < 0 && s <= e) ) {
+		animateflag = false;
+		return ;
+	}
+	s += step;
+	var cont = document.getElementById("pagecont");
+	cont.style.left = s + "px";
+	setTimeout("animate(" + s + "," + e + "," + step + ")", 1);	
+}
+
+/**
+ * 滚轮事件响应函数
+ * @param e 事件
+ */
+function wheelHandle(e) {
+	if(wheeltype(e) > 0) {
+		pagechange(1);
+	}else {
+		pagechange(-1);
+	}
+}
+
+/**
+ * 键盘事件响应函数
+ * @param e 事件
+ */
+function keyDownHandle(e) {
+	var evt = e || window.event;
+	if(evt.keyCode == 39) {
+		pagechange(-1);
+	}else if(evt.keyCode == 37) {
+		pagechange(1);
+	}
+};
+
+
+/**
+ * 注册滚轮事件函数
+ * @param element     : 注册的事件对象
+ * @param wheelHandle : 注册事件函数
+ */
+function addScrollListener(element, wheelHandle) {
+    if(typeof element != 'object') return;
+    if(typeof wheelHandle != 'function') return;
+    // 监测浏览器
+    if(typeof arguments.callee.browser == 'undefined') {
+        var user = navigator.userAgent;
+        var b = {};
+        b.opera = user.indexOf("Opera") > -1 && typeof window.opera == "object";
+        b.khtml = (user.indexOf("KHTML") > -1 || user.indexOf("AppleWebKit") > -1 || user.indexOf("Konqueror") > -1) && !b.opera;
+        b.ie = user.indexOf("MSIE") > -1 && !b.opera;
+        b.gecko = user.indexOf("Gecko") > -1 && !b.khtml;
+        arguments.callee.browser = b;
+    }
+    if(element == window)
+        element = document;
+    if(arguments.callee.browser.ie)
+        element.attachEvent('onmousewheel', wheelHandle);
+    else
+        element.addEventListener(arguments.callee.browser.gecko ? 'DOMMouseScroll' : 'mousewheel', wheelHandle, false);
+}
+
+/**
+ * 划动的类型
+ * -2表示没有开始触摸，0表示没有划动，1表示向左划，-1表示向右划
+ */
+var moveType = -2;
+/**
+ * 触摸起始点坐标
+ */
+var startX;
+
+function onTouchStart(e) {
+	if(e.touches.length != 1) {
+		return ;
+	}
+	moveType = 0;
+	startX = e.touches[0].clientX;
+}
+
+function onTouchMove(e) {
+//	alert("hehe");
+	e.preventDefault();
+	if(e.touches.length != 1 || moveType == -2) {
+		return ;
+	}
+	if (e.touches[0].clientX - startX > 10) {
+		moveType = -1;
+	}else if(startX - e.touches[0].clientX > 10) {
+		moveType = 1;
+	}
+}
+
+function onTouchEnd(e) {
+	//alert('he');
+	e.preventDefault();
+	if(moveType == 1 || moveType == -1) {
+		pagechange(-moveType);
+	}
+    moveType = -2;
+}
+
+function onTouchCancel(e) {
+    moveType = -2;
+}
