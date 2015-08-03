7a65c36aa434e098c0713c44ee5a395a69e7e730
Ben Xu <bjfuxuben@gmail.com>
2015-08-03 21:26:47 +0800

    转到github上

---
+++ content.js
@@ -0,0 +1,574 @@
+﻿var lists = new Array();
+
+var list = new Array();
+var title = "生活娱乐";
+var searchengine = new Array();
+searchengine.push({
+	url: "http://cn.bing.com/search",
+	name: "q",
+	title: "必应搜索"
+});
+searchengine.push({
+	url: "http://www.sogou.com/web",
+	name: "query",
+	title: "搜狗搜索"
+});
+list.push({
+	url: "http://wot.kongzhong.com/",
+	name: "坦克世界"
+});
+list.push({
+	url: "http://www.rubik.com.cn",
+	name: "魔方小站"
+});
+list.push({
+	url: "http://www.cuttherope.ie",
+	name: "割绳子"
+});
+list.push({
+	url: "http://bejeweled.popcap.com/html5/0.9.12.9490/html5/Bejeweled.html",
+	name: "宝石迷阵"
+});
+list.push({
+	url: "http://nus40g1.chinaw3.com/demos/fruit-ninja",
+	name: "水果忍者"
+});
+list.push({
+	url: "http://www.k618.cn",
+	name: "未来网"
+});
+lists.push({
+	title: title,
+	searchengine: searchengine,
+	list: list
+});
+
+var list = new Array();
+var title = "购物消费";
+var searchengine = new Array();
+searchengine.push({
+	url: "http://cn.bing.com/search",
+	name: "q",
+	title: "必应搜索"
+});
+searchengine.push({
+	url: "http://www.soso.com/q",
+	name: "w",
+	title: "腾迅搜搜"
+});
+list.push({
+	url: "http://map.sogou.com/",
+	name: "搜狗地图"
+});
+list.push({
+	url: "http://www.boc.cn/",
+	name: "中国银行"
+});
+list.push({
+	url: "http://www.icbc.com.cn/",
+	name: "工商银行"
+});
+list.push({
+	url: "http://www.abchina.com/cn/",
+	name: "农业银行"
+});
+list.push({
+	url: "http://www.10010.com/",
+	name: "中国联通"
+});
+list.push({
+	url: "http://hb.ct10000.com/",
+	name: "湖北电信"
+});
+list.push({
+	url: "http://www.jd.com",
+	name: "京东商城"
+});
+list.push({
+	url: "http://www.taobao.com/",
+	name: "淘宝网"
+});
+list.push({
+	url: "http://www.dangdang.com/",
+	name: "当当网"
+});
+list.push({
+	url: "http://www.amazon.cn/",
+	name: "亚马逊"
+});
+list.push({
+	url: "http://www.alipay.com/",
+	name: "支付宝"
+});
+list.push({
+	url: "http://ele.me/",
+	name: "饿了么"
+});
+list.push({
+	url: "http://www.4006365917.com/",
+	name: "悠淘网"
+});
+list.push({
+	url: "http://www.xiaomi.com",
+	name: "小米手机"
+});
+lists.push({
+	title: title,
+	searchengine: searchengine,
+	list: list
+});
+
+var list = new Array();
+var title = "生活社交";
+var searchengine = new Array();
+searchengine.push({
+	url: "http://cn.bing.com/search",
+	name: "q",
+	title: "必应搜索"
+});
+searchengine.push({
+	url: "http://www.soso.com/q",
+	name: "w",
+	title: "腾迅搜搜"
+});
+list.push({
+	url: "http://translate.google.com/",
+	name: "google翻译"
+});
+list.push({
+	url: "http://fanyi.youdao.com/",
+	name: "有道翻译"
+});
+list.push({
+	url: "http://gmail.google.com/",
+	name: "Gmail"
+});
+list.push({
+	url: "http://www.kuaipan.cn",
+	name: "金山快盘"
+});
+list.push({
+	url: "http://mail.bjfu.edu.cn",
+	name: "北林邮箱"
+});
+list.push({
+	url: "http://mail.sohu.com/",
+	name: "搜狐邮箱"
+});
+list.push({
+	url: "http://mail.163.com/",
+	name: "网易邮箱"
+});
+list.push({
+	url: "http://www.cnblogs.com/moonbay",
+	name: "博客园"
+});
+list.push({
+	url: "http://news.sina.com.cn/",
+	name: "新浪新闻"
+});
+list.push({
+	url: "http://wx.qq.com/",
+	name: "微信网页版"
+});
+list.push({
+	url: "http://web.qq.com/",
+	name: "webqq"
+});
+list.push({
+	url: "http://webim.feixin.10086.cn/",
+	name: "网页飞信"
+});
+list.push({
+	url: "http://m.renren.com",
+	name: "手机人人"
+});
+list.push({
+	url: "http://www.renren.com",
+	name: "人人网"
+});
+list.push({
+	url: "http://www.dajie.com/",
+	name: "大街网"
+});
+list.push({
+	url: "http://bbs.dengtian.com",
+	name: "登天"
+});
+list.push({
+	url: "http://wlan7.bj.chinaunicom.cn",
+	name: "联通Wlan"
+});
+lists.push({
+	title: title,
+	searchengine: searchengine,
+	list: list
+});
+
+var list = new Array();
+var title = "最常使用";
+var searchengine = new Array();
+searchengine.push({
+	url: "http://www.baidu.com/s",
+	name: "wd",
+	title: "百度搜索"
+});
+searchengine.push({
+	url: "http://www.google.com.hk/search",
+	name: "q",
+	title: "谷歌搜索"
+});
+list.push({
+	url: "http://jwc.bjfu.edu.cn",
+	name: "北林教务处"
+});
+list.push({
+	url: "http://graduate.bjfu.edu.cn",
+	name: "研究生院"
+});
+list.push({
+	url: "http://xxdt.bjfu.edu.cn",
+	name: "信息党团"
+});
+list.push({
+	url: "http://www.lib.bjfu.edu.cn",
+	name: "北林图书馆"
+});
+list.push({
+	url: "http://www.towords.com",
+	name: "拓词"
+});
+list.push({
+	url: "http://acm.bjfu.edu.cn/acmhome",
+	name: "北林oj"
+});
+list.push({
+	url: "http://code.google.com/codejam",
+	name: "code jam"
+});
+list.push({
+	url: "http://www.jd.com",
+	name: "京东商城"
+});
+list.push({
+	url: "http://www.taobao.com",
+	name: "淘宝网"
+});
+list.push({
+	url: "http://www.dangdang.com",
+	name: "当当网"
+});
+list.push({
+	url: "http://gmail.google.com",
+	name: "Gmail"
+});
+list.push({
+	url: "http://mail.bjfu.edu.cn",
+	name: "北林邮箱"
+});
+list.push({
+	url: "http://www.kuaipan.cn",
+	name: "金山快盘"
+});
+list.push({
+	url: "http://login.bjfu.edu.cn",
+	name: "连接网络"
+});
+list.push({
+	url: "http://translate.google.com/",
+	name: "google翻译"
+});
+list.push({
+	url: "http://fanyi.youdao.com/",
+	name: "有道翻译"
+});
+list.push({
+	url: "http://www.towords.com",
+	name: "拓词"
+});
+list.push({
+	url: "http://mail.bjfu.in/",
+	name: "A协邮箱"
+});
+list.push({
+	url: "http://www.cnblogs.com/moonbay",
+	name: "博客园"
+});
+list.push({
+	url: "http://www.52v6.com/ptflicker/",
+	name: "52v6网"
+});
+list.push({
+	url: "http://bt.neu6.edu.cn/",
+	name: "六维空间"
+});
+list.push({
+	url: "http://wlan7.bj.chinaunicom.cn",
+	name: "联通Wlan"
+});
+lists.push({
+	title: title,
+	searchengine: searchengine,
+	list: list
+});
+
+var list = new Array();
+var title = "学习事务";
+var searchengine = new Array();
+searchengine.push({
+	url: "http://www.youdao.com/search",
+	name: "q",
+	title: "有道搜索"
+});
+searchengine.push({
+	url: "http://scholar.google.com/scholar",
+	name: "q",
+	title: "谷歌学术搜索"
+});
+list.push({
+	url: "http://graduate.bjfu.edu.cn",
+	name: "研究生院"
+});
+list.push({
+	url: "http://jwc.bjfu.edu.cn",
+	name: "北林教务处"
+});
+list.push({
+	url: "http://news.bjfu.edu.cn",
+	name: "绿色新闻网"
+});
+list.push({
+	url: "http://xxdt.bjfu.edu.cn",
+	name: "信息党团"
+});
+list.push({
+	url: "http://www.bjfu.edu.cn",
+	name: "北林主页"
+});
+list.push({
+	url: "http://www.lib.bjfu.edu.cn",
+	name: "北林图书馆"
+});
+list.push({
+	url: "http://nic.bjfu.edu.cn",
+	name: "信息中心"
+});
+list.push({
+	url: "http://sports.bjfu.edu.cn",
+	name: "体育教学部"
+});
+list.push({
+	url: "http://xgxt.bjfu.edu.cn",
+	name: "学工系统"
+});
+list.push({
+	url: "http://job.bjfu.edu.cn",
+	name: "就业信息网"
+});
+list.push({
+	url: "http://soft.bjfu.edu.cn",
+	name: "免费软件"
+});
+list.push({
+	url: "http://www.towords.com",
+	name: "拓词"
+});
+list.push({
+	url: "http://www.shaorongwang.com",
+	name: "王少荣"
+});
+lists.push({
+	title: title,
+	searchengine: searchengine,
+	list: list
+});
+
+var list = new Array();
+var title = "科研开发";
+var searchengine = new Array();
+searchengine.push({
+	url: "http://academic.research.microsoft.com/Search",
+	name: "query",
+	title: "微软学术搜索"
+});
+searchengine.push({
+	url: "http://scholar.google.com/scholar",
+	name: "q",
+	title: "谷歌学术搜索"
+});
+list.push({
+	url: "http://www.pudn.com",
+	name: "程序员网"
+});
+list.push({
+	url: "http://www.csdn.net/",
+	name: "csdn"
+});
+list.push({
+	url: "http://javacc.dev.java.net/",
+	name: "javacc"
+});
+list.push({
+	url: "http://www.cplusplus.com/reference/algorithm/",
+	name: "C++参考"
+});
+list.push({
+	url: "http://www.freewebsitetemplates.com",
+	name: "免费网站模板"
+});
+list.push({
+	url: "http://sc.chinaz.com/tubiao",
+	name: "图标下载"
+});
+list.push({
+	url: "http://ipv6.torrent.ubuntu.com/",
+	name: "ubuntu官网"
+});
+list.push({
+	url: "http://refresh-sf.com/yui/",
+	name: "前端代码压缩"
+});
+list.push({
+	url: "http://ie.microsoft.com/testdrive/",
+	name: "Test Drive"
+});
+list.push({
+	url: "http://www.oray.com/",
+	name: "oray"
+});
+list.push({
+	url: "http://www.oot.cn/",
+	name: "网信"
+});
+list.push({
+	url: "http://www.zhujiwu.com/",
+	name: "主机屋"
+});
+list.push({
+	url: "http://www.aliyun.com/",
+	name: "阿里云"
+});
+list.push({
+	url: "http://sbcx.saic.gov.cn/trade/index.jsp",
+	name: "中国商标网"
+});
+list.push({
+	url: "http://localhost/ccb/",
+	name: "本机ccb"
+});
+list.push({
+	url: "http://localhost/ben/",
+	name: "本机ben"
+});
+list.push({
+	url: "http://acm.bjfu.edu.cn/ben/",
+	name: "acm服务器"
+});
+list.push({
+	url: "http://bjfuxuben.vicp.net/ccb/",
+	name: "生物计算"
+});
+list.push({
+	url: "http://bjfuxuben.vicp.net:8080/",
+	name: "曙光大型机"
+});
+list.push({
+	url: "http://groups.google.com/group/hustoj/",
+	name: "HUSTOJ讨论版"
+});
+lists.push({
+	title: title,
+	searchengine: searchengine,
+	list: list
+});
+
+var list = new Array();
+var title = "算法竞赛";
+var searchengine = new Array();
+searchengine.push({
+	url: "http://academic.research.microsoft.com/Search",
+	name: "query",
+	title: "微软学术搜索"
+});
+searchengine.push({
+	url: "http://scholar.google.com/scholar",
+	name: "q",
+	title: "谷歌学术搜索"
+});
+list.push({
+	url: "http://acm.bjfu.edu.cn/acmhome",
+	name: "北林oj"
+});
+list.push({
+	url: "http://acm.hdu.edu.cn",
+	name: "杭电oj"
+});
+list.push({
+	url: "http://poj.org",
+	name: "北大oj"
+});
+list.push({
+	url: "http://acm.bupt.edu.cn/onlinejudge",
+	name: "北邮oj"
+});
+list.push({
+	url: "http://acm.zju.edu.cn/onlinejudge/",
+	name: "浙大oj"
+});
+list.push({
+	url: "http://acm.nuaa.edu.cn/",
+	name: "南航oj"
+});
+list.push({
+	url: "http://acm.hrbeu.edu.cn/",
+	name: "哈工程oj"
+});
+list.push({
+	url: "http://www.bianchengla.com/",
+	name: "北航编程啦"
+});
+list.push({
+	url: "http://acm.fzu.edu.cn/",
+	name: "福州大学oj"
+});
+list.push({
+	url: "http://cs.scu.edu.cn/",
+	name: "四川大学oj"
+});
+list.push({
+	url: "http://acm-icpc.cn/AsiaSites_cn.html",
+	name: "acm中国"
+});
+list.push({
+	url: "http://cm.baylor.edu/",
+	name: "acm官网"
+});
+list.push({
+	url: "http://astar.baidu.com/",
+	name: "百度之星"
+});
+list.push({
+	url: "http://www.zjtg.cn/itjs/",
+	name: "信息学竞赛"
+});
+list.push({
+	url: "http://www.apfloat.org/apfloat/",
+	name: "高效算法库"
+});
+list.push({
+	url: "http://code.google.com/codejam",
+	name: "code jam"
+});
+list.push({
+	url: "http://www.acmwiki.com/",
+	name: "ACM百科"
+});
+list.push({
+	url: "http://oeis.org/",
+	name: "公式推导"
+});
+lists.push({
+	title: title,
+	searchengine: searchengine,
+	list: list
+});
---
+++ index.jsp
@@ -0,0 +1,41 @@
+﻿<%@ page language="java" contentType="text/html; charset=utf-8"
+	pageEncoding="utf-8"%>
+<!doctype html>
+<html>
+	<head>
+		<title>我的网址导航</title>
+		<meta content="导航" name="keywords"/>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<link rel=stylesheet href="index.css" type="text/css">
+		<script type="text/javascript" src="dateandtime.js" charset="utf-8">
+		</script>
+		<script type="text/javascript" src="scroll.js" charset="utf-8">
+		</script>
+		<script type="text/javascript" src="client.js" charset="utf-8">
+		</script>
+		<script type="text/javascript" src="content.js" charset="utf-8">
+		</script>
+		<script type="text/javascript" src="index.js" charset="utf-8">
+		</script>
+	</head>
+	<body>
+		<div id = "wrapper">
+			<div id = "pagewindow">
+				<div id = "pagecont">
+				</div>
+			</div>
+			<br/>
+			<div align="center" id="bottom">
+				<span id="mydate">
+					<script type="text/javascript">
+			            var date = new ChineseCalendar();
+            			document.write(date.YYMMDD() + " " + date.weekday() + " " + date.solarDay2());
+			        </script>	
+				</span>
+				<span id="mytime"></span>
+				<span> 本机外网ip:<%=request.getRemoteAddr()%></span>
+			</div>
+		</div>
+	</body>
+</html>
